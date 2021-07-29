0cca9a89b8ea3f735f8e6e885110dc31b1f833f8
adrianaferrari <adriana.ferrari997@gmail.com>
2021-07-29 17:54:53 +0200

    change font

--- frontend/src/startup/chartjs.ts
+++ frontend/src/startup/chartjs.ts
@@ -4,4 +4,6 @@ Chart.defaults.elements.point.radius = 5;
 Chart.defaults.elements.point.hoverRadius = 5;
 Chart.defaults.elements.point.hitRadius = 5;
 Chart.defaults.maintainAspectRatio = false;
+Chart.defaults.font.family = 'Rubik';
+Chart.defaults.font.size = 13;
 Chart.overrides.pie.aspectRatio = 2;
